a10d3552fe78a641ea793c0d6bd4089fc5d567e0
ALEYI <aleelaleyi@gmail.com>
2025-04-30 16:15:20 -0500

    Create a connection to clickhouse

---
+++ internal/clickhouse/conection.go
@@ -0,0 +1,52 @@
+package clickhouse
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+)
+
+type Chconnection struct {
+	conn driver.Conn
+}
+
+func NewConnection(ctx context.Context) (*Chconnection, error) {
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{":9000"},
+		Auth: clickhouse.Auth{
+			Database: "audit",
+			Username: "user",
+			Password: "password",
+		},
+		ClientInfo: clickhouse.ClientInfo{
+			Products: []struct {
+				Name    string
+				Version string
+			}{
+				{Name: "an-example-go-client", Version: "0.1"},
+			},
+		},
+		Debugf: func(format string, v ...interface{}) {
+			fmt.Printf(format, v)
+		},
+		TLS: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		return nil, err
+	}
+
+	return &Chconnection{conn: conn}, nil
+}
