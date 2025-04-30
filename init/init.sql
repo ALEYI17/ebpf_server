adaf54e8f0b67b09128bb40e04208f8fcb09caf0
ALEYI <aleelaleyi@gmail.com>
2025-04-30 13:53:25 -0500

    Create files to test clickhouse

---
+++ init/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS audit;
+
+CREATE TABLE IF NOT EXISTS audit.tracing_events (
+  pid UInt32,
+  uid UInt32,
+  comm String,
+  filename String,
+  timestamp_ns_exit UInt64,
+  return_code Int64,
+  timestamp_ns UInt64,
+  latency_ns UInt64
+) ENGINE = MergeTree()
+ORDER BY timestamp_ns;
